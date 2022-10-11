46c407b9dd2d6f5758e9632d6293121f26d94027
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2022-10-11 21:44:43 +0600

    1st process

--- scripts.js
+++ scripts.js
@@ -1,1 +1,37 @@
-alert("ok")
+var date = new Date;
+
+var toYear = date.getFullYear();
+document.getElementById("toYear").value=toYear;
+
+var toMonth = date.getMonth()-1;
+document.getElementById("toMonth").value=toMonth;
+
+var toDate = date.getDate();
+document.getElementById("toDate").value=toDate;
+
+function cal(){
+    var bYear = parseInt(document.getElementById("bYear").value);
+    var bMonth = parseInt(document.getElementById("bMonth").value);
+    var bDate = parseInt(document.getElementById("bDate").value);
+
+   if(toDate<bDate){
+    toDate = toDate +30;
+    toDate = (toDate-bDate);
+    toMonth = (toMonth -1);
+    if(toMonth<bMonth){
+        toMonth =((toMonth +12)-bMonth);
+        toYear = ((toYear -1)-bYear);
+
+    }
+    else{
+
+        toMonth =(toMonth -bMonth);
+        toYear = (toYear -bYear);
+
+
+    }
+   }
+   document.getElementById("rDate").value=toDate;
+   document.getElementById("rMonth").value=toMonth;
+   document.getElementById("rYear").value=toYear;
+}
